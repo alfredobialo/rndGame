efacaa28a39fd900ca7895f7f213634e187fd888
Alfred Obialo <alfredcsdinc@gmail.com>
2026-05-28 23:19:19 +0100

    First Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template: `
+    <div class="bg-amber-100">
+      <h1 class="text-2xl text-amber-700">The Game App</h1>
+    </div>
+
+
+  `,
+  styles: ``
 })
 export class App {
   protected readonly title = signal('rndGame');
